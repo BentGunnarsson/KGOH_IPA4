cd46bc579ab7ee195f2827d87b9ea95d360a04ae
BentGunnarsson <bent19@ru.is>
2020-03-10 18:45:28 +0000

    Refactor2

--- sample.py
+++ sample.py
@@ -1,2 +1,7 @@
 def FizzBuzz(num):
+    ret_str = ""
+    if num % 3 == 0:
+        ret_str += "Fizz"
+        return ret_str
+        
     return num
--- test_sample.py
+++ test_sample.py
@@ -2,3 +2,6 @@ import sample
 
 def test_return_number():
     assert sample.FizzBuzz(7) == 7
+
+def test_return_fizz():
+    assert sample.FizzBuzz(6) == "Fizz"
